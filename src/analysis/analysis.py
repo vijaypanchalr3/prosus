b6da18a4109fec5ce514b6950d5800bcaca7cb06
vijay panchal <vijay@debian.deb>
2024-02-27 23:57:00 +0530

    analysis showable

--- src/analysis/analysis.py
+++ src/analysis/analysis.py
@@ -22,7 +22,7 @@ class Result:
         samplefe2o3=tools.data("../data/sample1.csv")
         samplenickel=tools.data("../data/sample2nickel.csv")
 
-
+        self.load_first_time=True
         self.header = background1.header
         print(self.header)
         self.arrays_bg1 = background1.arrays
@@ -49,29 +49,37 @@ class Result:
         fig.suptitle(title)
         plt.subplots_adjust(top=0.85)
         plt.subplots_adjust(bottom=.15)
-        fig.savefig(filename,transparent=True,format="svg")
+        fig.savefig(filename,transparent=False,format="svg")
 
     def write_results(self,arrays,filename,title):
-        arrays.pop(0)
-        print(arrays)
-        exit()
-        for i in range(4):
-            with open(filename,"a",newline="") as file:
-                writer = csv.writer(file)
-                writer.writerow([title,self.header[i],np.mean(arrays[i]),np.median(arrays[i]),np.std(arrays[i]),np.var(arrays[i]), np.min(arrays[i]),np.max(arrays[i])])
+        if self.load_first_time:
+            with open(filename,"w",newline="") as file:    
+                writer =csv.writer(file)
+                writer.writerow(["Title", "Params", "Mean", "Median", "std", "variance", "min", "max"])
+            self.load_first_time=False
+            for i in range(4):
+                with open(filename,"a",newline="") as file:
+                    writer = csv.writer(file)
+                    writer.writerow([title,self.header[i],np.mean(arrays[i]),np.median(arrays[i]),np.std(arrays[i]),np.var(arrays[i]), np.min(arrays[i]),np.max(arrays[i])])
+
+        else:
+            for i in range(4):
+                with open(filename,"a",newline="") as file:
+                    writer = csv.writer(file)
+                    writer.writerow([title,self.header[i],np.mean(arrays[i]),np.median(arrays[i]),np.std(arrays[i]),np.var(arrays[i]), np.min(arrays[i]),np.max(arrays[i])])
 
 
 
 MASTER = Result()
-# MASTER.plot_hist(MASTER.arrays_bg1, "bg1.svg", "Background 1")
-# MASTER.plot_hist(MASTER.arrays_bg2, "bg2.svg", "Background 2")
-# MASTER.plot_hist(MASTER.arrays_bg3, "bg3.svg", "Background 3")
-# MASTER.plot_hist(MASTER.arrays_bg4, "bg3.svg", "Background 4")
-# MASTER.plot_hist(MASTER.arrays_s1, "s1.svg", "sample 1")
-# MASTER.plot_hist(MASTER.arrays_s2, "s2.svg", "sample 2")
-MASTER.write_results(MASTER.arrays_bg1,"result.csv","Background 1")
-MASTER.write_results(MASTER.arrays_bg2,"result.csv","Background 2")
-MASTER.write_results(MASTER.arrays_bg3,"result.csv","Background 3")
-MASTER.write_results(MASTER.arrays_bg4,"result.csv","Background 4")
-MASTER.write_results(MASTER.arrays_s1,"result.csv","Sample 1")
-MASTER.write_results(MASTER.arrays_s2,"result.csv","Sample 2")
+MASTER.plot_hist(MASTER.arrays_bg1, "bg1.svg", "Background 1")
+MASTER.plot_hist(MASTER.arrays_bg2, "bg2.svg", "Background 2")
+MASTER.plot_hist(MASTER.arrays_bg3, "bg3.svg", "Background 3")
+MASTER.plot_hist(MASTER.arrays_bg4, "bg3.svg", "Background 4")
+MASTER.plot_hist(MASTER.arrays_s1, "s1.svg", "sample 1")
+MASTER.plot_hist(MASTER.arrays_s2, "s2.svg", "sample 2")
+MASTER.write_results(MASTER.arrays_bg1,"results.csv","Background 1")
+MASTER.write_results(MASTER.arrays_bg2,"results.csv","Background 2")
+MASTER.write_results(MASTER.arrays_bg3,"results.csv","Background 3")
+MASTER.write_results(MASTER.arrays_bg4,"results.csv","Background 4")
+MASTER.write_results(MASTER.arrays_s1,"results.csv","Sample 1")
+MASTER.write_results(MASTER.arrays_s2,"results.csv","Sample 2")
